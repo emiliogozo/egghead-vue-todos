90c5f849792b0a275f660504649b9c454a091cde
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-04-15 14:23:57 +0800

    feat: Manage state with Vuex ans Nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>
-        Hello world
-    </div>
+  <div>
+    {{counter}}
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      counter: state => state.counter
+    })
+  }
+}
+</script>
